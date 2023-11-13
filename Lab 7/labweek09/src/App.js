f4d7a849752ea7ce7e8bb05a4a676a82cb2abfe6
mlbano <mlbano26@gmail.com>
2023-11-12 21:43:09 -0500

    created lab 7

---
+++ Lab 7/labweek09/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h2>Welcome to Fullstack Development - I</h2>
+          <h3>React JS Programming Week09 Lab exercise</h3>
+          <h4>101355992</h4>
+          <h5>Milanie Bano</h5>
+          <h6>George Brown College, Toronto</h6>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
